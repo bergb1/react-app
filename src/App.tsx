854f13b649ffa0616dec3bad8c55bdaf130449bb
Brian van den Berg <brianvdberg2107@gmail.com>
2023-04-28 00:11:35 +0300

    Removed cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import Home from "./Home";
 import Auth from "./Auth";
-import Cookie from "./interfaces/cookie";
 import { useState } from "react";
 
 const app = () => {
-  const cookie = JSON.parse(document.cookie) as Cookie;
-  console.log(`session: ${cookie.token}`);
-
-  const [auth, setAuth] = useState(cookie.token);
-
+  const [auth, setAuth] = useState('');
+  
   return <>{auth ? <Home auth={setAuth} /> : <Auth callback={setAuth} />}</>;
 };
 
