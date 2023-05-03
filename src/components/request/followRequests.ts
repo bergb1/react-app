3569a05326b9e7847015a60f4d6449e957b6a8bf
Brian van den Berg <brianvdberg2107@gmail.com>
2023-05-03 14:57:50 +0300

    follower and following requests

--- src/components/request/followRequests.ts
+++ src/components/request/followRequests.ts
@@ -50,4 +50,44 @@ const followerRemove = async (token: string, _id: string) => {
   );
 };
 
-export { followerAdd, followerRemove };
+// Request to get the followers of a user
+const followers = async (_id: string) => {
+  // Define the query
+  const query = `query Query($id: String!) {
+    followers(_id: $id)
+  }`;
+
+  // Define the variables
+  const variables = {
+    id: _id,
+  };
+
+  // Process the request
+  return await fetch(url, setupFetch(query, undefined, variables)).then(
+    async (response) => {
+      return (await handleFetch(response)).followers;
+    }
+  );
+};
+
+// Request to get the users ID's a user is following
+const following = async (_id: string) => {
+  // Define the query
+  const query = `query Query($id: String!) {
+    following(_id: $id)
+  }`;
+
+  // Define the variables
+  const variables = {
+    id: _id,
+  };
+
+  // Process the request
+  return await fetch(url, setupFetch(query, undefined, variables)).then(
+    async (response) => {
+      return (await handleFetch(response)).followers;
+    }
+  );
+};
+
+export { followerAdd, followerRemove, followers, following };
